3084255533818666fd7b9331f3c48c0622dade6e
SunStar56 <league@WTS-IM-17.attlocal.net>
2017-07-02 13:40:58 -0700

    Netflix
    7/2/17
    Finished! ^-^

---
+++ src/Netflix.java
@@ -0,0 +1,25 @@
+public class Netflix {
+	public static void main(String[] args) {
+		Movie tw = new Movie("Twilight", 2);
+		Movie hp = new Movie("Harry Potter", 5);
+		Movie lotr = new Movie("Lord of the Rings", 4);
+		Movie doc = new Movie("Water - A Documentary", 3);
+		Movie rhf = new Movie("Random Horror Film", 1);
+		System.out.println(tw.getTicketPrice());
+		System.out.println(hp.getTicketPrice());
+		System.out.println(lotr.getTicketPrice());
+		System.out.println(doc.getTicketPrice());
+		System.out.println(rhf.getTicketPrice());
+
+		NetflixQueue nq = new NetflixQueue();
+		nq.addMovie(tw);
+		nq.addMovie(hp);
+		nq.addMovie(lotr);
+		nq.addMovie(doc);
+		nq.addMovie(rhf);
+		nq.sortMoviesByRating();
+		nq.printMovies();
+		System.out.println("The best movie is " + nq.getBestMovie() + ".");
+		System.out.println("The second best movie is " + nq.getMovie(1) + ".");
+	}
+}
